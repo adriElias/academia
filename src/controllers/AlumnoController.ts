7c29beb8c521a2a8e02952a40ca68f6ee7d780db
adriElias <34091624+adriElias@users.noreply.github.com>
2025-07-26 20:01:36 +0200

    Add method inscribirCurso in AlumnoController

--- src/controllers/AlumnoController.ts
+++ src/controllers/AlumnoController.ts
@@ -9,5 +9,20 @@ export class AlumnoController extends NocodbController {
     getAlumnosCurso(idCurso: string | number): void {
         // ...
     }
-    
+
+    async inscribirCurso(idCurso: string | number, idAlumno: string | number): Promise<boolean> {
+        const apiUrl = `https://app.nocodb.com/api/v2/tables/met3zt25o9idyyf/links/cjox6viwllvumga/records/${idAlumno}`
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify([{Id: idCurso}])
+        });
+        const data = await response.json();
+        return data as boolean;
+
+    }
+
 }
